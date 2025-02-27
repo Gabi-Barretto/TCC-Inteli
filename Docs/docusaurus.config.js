9e061b5feddf4f865ba2ea41e2068152591d136b
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2025-02-27 17:21:30 -0300

    Update - Docusaurus

--- Docs/docusaurus.config.js
+++ Docs/docusaurus.config.js
@@ -1,12 +1,4 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
-import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -14,78 +6,52 @@ const config = {
   tagline: 'Agricultural Remote Monitoring',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://gabi-barretto.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/TCC-Inteli/',
+  url: 'https://Gabi-Barretto.github.io', // URL do GitHub Pages
+  baseUrl: '/TCC-Inteli/', // Nome do repositório com barra no final
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'gabi-barretto', // Usually your GitHub org/user name.
-  projectName: 'TCC-Inteli', // Usually your repo name.
+  organizationName: 'Gabi-Barretto', // Nome da conta no GitHub
+  projectName: 'TCC-Inteli', // Nome do repositório
+  deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  trailingSlash: false, // Pode evitar problemas de roteamento
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+  trailingSlash: false,
 
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
-          routeBasePath: 'docs', // Faz com que a documentação seja servida na raiz
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:'',
+          path: 'docs',
+          routeBasePath: '/', // Servir docs na raiz
+          sidebarPath: require.resolve('./sidebars.js'),
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'F-ARM',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+  themeConfig: {
+    navbar: {
+      title: 'F-ARM',
+      logo: {
+        alt: 'Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentação',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Documentação',
-          }
-        ],
-      },
-      footer: {
-        style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} F-ARM.`,
+    },
+  },
 };
 
 export default config;
